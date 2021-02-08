d5b5d18580ebd2f97308d9a126fdfc22c316bf4a
CMACCKK <62099095+CMACCKK@users.noreply.github.com>
2021-02-08 20:09:27 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 chrome_path = '/opt/google/chrome/google-chrome'    # chrome安装路径
 crawlergo_threads = "10"                        # crawlergo线程
-zoomeye_username = "emailforgty@163.com"        # Zoomeye账号
-zoomeye_password = "gty991214"                  # Zoomeye密码
+zoomeye_username = "xxxxx@163.com"        # Zoomeye账号
+zoomeye_password = ""                  # Zoomeye密码
 login_url = 'https://api.zoomeye.org/user/login'                                            # Zoomeye的登陆地址
 unscan_suffix = ['.jpg', '.png', '.gif', '.js', '.css', '.ico', '.txt', '.gov.cn']          # 不扫描的文件后缀
